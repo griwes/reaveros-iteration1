cdf2313f668df1a2af1bff769bed1f6f52e9c68e
Michał 'Griwes' Dominiak <griwes@griwes.info>
2013-01-20 17:16:33 +0100

    Fixed VMM prototype a bit.

--- services/vmm/main.cpp
+++ services/vmm/main.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include <map>
 #include <string>
 
 #include <rose/rose.h>
@@ -10,7 +10,7 @@ namespace rose
 {
     namespace vmm
     {
-        std::vector<rose::vmm::address_space> address_spaces;
+        std::map<rose::process, rose::vmm::address_space> address_spaces;
         std::pair<uint64_t, uint64_t> allowed_addresses;
     }
 }
@@ -20,6 +20,7 @@ int main()
     rose::vmm::allowed_addresses = rose::get_allowed_address_range();
     
     rose::register_handler(rose::ipc, rose::pool(16), rose::vmm::receive);
+    rose::service::register_handler(rose::service::pf, rose::pool(16), rose::vmm::handle_pf);
     
     while (rose::get_signal() != rose::signal::term)
     {
