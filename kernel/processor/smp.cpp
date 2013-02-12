ae5bcf47d0115674603b66b01ef5accfae06d026
Michał 'Griwes' Dominiak <griwes@griwes.info>
2013-02-12 16:32:36 +0100

    Erm.

--- kernel/processor/smp.cpp
+++ kernel/processor/smp.cpp
@@ -49,7 +49,7 @@ void processor::smp::boot(core * cores, uint64_t num_cores)
     
     for (uint64_t i = 0; i < num_cores; ++i)
     {
-        memory::copy(trampoline_start, (uint8_t *)0x1000 + trampoline_size, trampoline_size);
+        memory::copy(trampoline_start, (uint8_t *)0x1000 + trampoline_size * i, trampoline_size);
         cores[i].started = (uint8_t *)(trampoline_start + trampoline_size * i);
     }
     
