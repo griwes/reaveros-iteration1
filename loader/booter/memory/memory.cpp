b2e3cf53e99caad7ebc9e4f31f847aeac08a13d2
Michał 'Griwes' Dominiak <griwes@griwes.info>
2013-02-16 01:52:40 +0100

    I don't need any 16KiB stacks, it's µkernel, not a monsterlithic one.

--- loader/booter/memory/memory.cpp
+++ loader/booter/memory/memory.cpp
@@ -94,13 +94,13 @@ uint64_t memory::install_kernel(uint32_t kernel_base, uint32_t kernel_length)
 void memory::install_kernel_stack(uint64_t & kernel_end)
 {
     memory::default_allocator.align(4096);
-    uint8_t * kernel_stack = new uint8_t[4 * 4096];
+    uint8_t * kernel_stack = new uint8_t[4096];
     
     kernel_end += 4096;
     
-    vas->map(kernel_end, kernel_end + 4 * 4096, (uint64_t)kernel_stack);
+    vas->map(kernel_end, kernel_end + 4096, (uint64_t)kernel_stack);
     
-    kernel_end += 4 * 4096;
+    kernel_end += 4096;
 }
 
 void memory::install_initrd(uint64_t kernel_end, uint32_t initrd_base, uint32_t initrd_length)
