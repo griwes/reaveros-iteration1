20255f95c44a836d5b93ab11d69bf863a9ab5e10
Michał 'Griwes' Dominiak <griwes@griwes.info>
2013-07-12 21:25:58 +0200

    binutils: 2.22 -> 2.23.2
    Let's hope it doesn't break anything. :D

--- utils/build-tools.sh
+++ utils/build-tools.sh
@@ -4,9 +4,9 @@ mkdir -p .binutils
 export PREFIX=$(pwd)/utils/tools
 export TARGET=x86_64-elf
 cd .binutils
-wget http://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2
-tar jxf binutils-2.22.tar.bz2
-mv binutils-2.22 binutils
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.bz2
+tar jxf binutils-2.23.2.tar.bz2
+mv binutils-2.23.2 binutils
 mkdir build
 cd build
 ../binutils/configure --target=$TARGET --prefix=$PREFIX --disable-nls
