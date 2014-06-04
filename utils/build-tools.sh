46fec7558ca154b8d1d0ccb3cd973ef840b45be2
Michał Dominiak <griwes@griwes.info>
2014-06-04 13:51:47 +0200

    Temporarily use gcc on binutils.
    This is supposed to avoid latest clang crashing on ieee.c. To be investigated.
    
    Also let's test 2.24 soon.

--- utils/build-tools.sh
+++ utils/build-tools.sh
@@ -5,12 +5,14 @@ mkdir -p utils/tools
 mkdir -p .binutils
 export PREFIX=$(pwd)/utils/tools
 export TARGET=x86_64-elf
-export CC=clang
-export CXX=clang++
+#export CC=clang
+#export CXX=clang++
+export CC=gcc
+export CXX=g++
 cd .binutils
-wget http://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2
-tar jxf binutils-2.22.tar.bz2
-mv binutils-2.22 binutils
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2
+tar jxf binutils-2.24.tar.bz2
+mv binutils-2.24 binutils
 mkdir build
 cd build
 ../binutils/configure --target=$TARGET --prefix=$PREFIX --disable-nls --disable-werror
