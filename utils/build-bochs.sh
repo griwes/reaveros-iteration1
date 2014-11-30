e5e9ccb0c58d51f04f6fa5ca4e581d4f8c021856
Michał 'Griwes' Dominiak <griwes@griwes.info>
2014-11-30 14:05:10 +0100

    Update .gitignore and build-bochs to link pthread.

--- utils/build-bochs.sh
+++ utils/build-bochs.sh
@@ -7,7 +7,7 @@ cd bochs-2.6.2
 ./configure --enable-smp --enable-cpu-level=6 --enable-all-optimizations --enable-x86-64 --enable-pci --enable-vmx \
     --enable-debugger --enable-disasm --enable-debugger-gui --enable-logging --enable-fpu --enable-3dnow \
     --enable-sb16=dummy --enable-cdrom --enable-x86-debugger --enable-iodebug --disable-plugins --disable-docbook \
-    --with-x --with-x11 --with-term
-make
+    --with-x --with-x11 --with-term LDFLAGS='-pthread'
+make -j3 -l6
 sudo make install
 cd ../..
